e20804f321f66d50e9e4e686de74a1b152efa3b4
Blizz Softs <cagoh55@gmail.com>
2023-07-18 17:03:45 +0800

    18.7.2023

---
+++ ChrisCourses/platformer/Multi-room/index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 64 * 16 // 1024
+canvas.height = 64 * 9 // 576
+
+let y = 100
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    c.fillStyle = 'red'
+    c.fillRect(100, y, 100, 100)
+    y++
+}
+
+animate()
